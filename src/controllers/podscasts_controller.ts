f4cdbe798d6ba8559578a6fe104e4741f98490c5
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-27 16:22:32 -0300

    creating Service

--- src/controllers/podscasts_controller.ts
+++ src/controllers/podscasts_controller.ts
@@ -1,24 +1,14 @@
-import {IncomingMessage, ServerResponse} from "http";
+import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from '../services/List_Episodes_Service'
 
 export const getListEpisodes = async(
     req: IncomingMessage,
     res: ServerResponse
 ) => {
+
+    const content = await serviceListEpisodes()
     res.writeHead(200, {'content-type': "application/json"});
     res.end(
-        JSON.stringify([
-            {
-              "podcastName": "flow",
-              "episode": "CBUM - Flow #319",
-              "videoId": "pQSuQmUfS30",
-              "categories": ["saúde", "esporte", "bodybuilder"]
-            },
-            {
-              "podcastName": "flow",
-              "episode": "RUBENS BARRICHELLO - Flow #339",
-              "videoId": "4KDGTdiOV4I",
-              "categories": ["esporte", "corrida"]
-            }
-          ])
+        JSON.stringify(content)
     );
 };
