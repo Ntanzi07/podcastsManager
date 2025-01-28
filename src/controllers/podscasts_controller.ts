e1e44a73b65d4071a4efaa52d24c2ef7f08f9409
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-28 14:54:32 -0300

    Update podscasts_controller.ts, creating ServiceFilterEpisodes()

--- src/controllers/podscasts_controller.ts
+++ src/controllers/podscasts_controller.ts
@@ -1,14 +1,25 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { serviceListEpisodes } from '../services/List_Episodes_Service'
+import { serviceListEpisodes } from '../services/list_episodes_service'
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
 export const getListEpisodes = async(
     req: IncomingMessage,
     res: ServerResponse
 ) => {
-
     const content = await serviceListEpisodes()
     res.writeHead(200, {'content-type': "application/json"});
     res.end(
         JSON.stringify(content)
     );
 };
+
+export const getFilterEpsodes = (
+    req: IncomingMessage,
+    res: ServerResponse
+) => {
+    const content = await serviceFilterEpisodes('flow');
+    res.writeHead(200, {'content-type': "application/json"});
+    res.end(
+        JSON.stringify(content)
+    )
+}
